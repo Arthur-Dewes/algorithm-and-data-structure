5f5e4213986c703d6a169a38b7d7924156ac184a
Arthur-Dewes <arthurgdewes@gmail.com>
2025-06-06 08:25:07 -0500

    DSA

--- sorting_algorithms/merge/merge2.py
+++ sorting_algorithms/merge/merge2.py
@@ -1,50 +1,26 @@
-def mergeSort(data: list[int]) -> list[int]:
-    """
-    This function determines whether the list is broken
-    into individual parts
-    """
-    if len(data) < 2:
-        return data
+def mergeSort(l:list[int]):
+    arr = l[::]
+    if len(arr) < 2:
+        return arr
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+    return merge(mergeSort(left), mergeSort(right))
 
-    middle = len(data)//2
-    left = mergeSort(data[:middle])
-    right = mergeSort(data[middle:])
-    merged = merge(left, right)
-
-    return merged
-
-def merge(left, right):
-    """
-    When left side/right side is empty, 
-    It means that this is an individual item and is already sorted.
-    """
-
-    # We make sure the right/left side is not empty
-    # meaning that it's an individual item and it's already sorted.
-    if not len(left):
-        return left
-
-    if not len(right):
-        return right
-
-    result = []
-    leftIndex = 0
-    rightIndex = 0
-    totalLen = len(left) + len(right)
-
-    while (len(result) < totalLen):
-
-        # Perform the required comparisons and merge the two parts
-
-        if left[leftIndex] < right[rightIndex]:
-            result.append(left[leftIndex])
-            leftIndex += 1
+def merge(l, r):
+    arr = []
+    i = j = 0
+    while i < len(l) and j < len(r):
+        if l[i] <= r[j]:
+            arr.append(l[i])
+            i += 1
         else:
-            result.append(right[rightIndex])
-            rightIndex += 1
-
-        if leftIndex == len(left) or rightIndex == len(right):
-            result.extend(left[leftIndex:] or right[rightIndex:])
-            break
-
-    return result
+            arr.append(r[j])
+            j += 1
+    while i < len(l):
+        arr.append(l[i])
+        i += 1
+    while j < len(r):
+        arr.append(r[j])
+        j += 1
+    return arr
